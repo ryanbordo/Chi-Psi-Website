2076cad1ad0b44e83b6ed744cb0bc972e43b6b45
Raymond Zhang <yz2568@cornell.edu>
2022-11-28 17:03:24 -0500

    Raymond - changing background opacity

--- scripts/brothers.js
+++ scripts/brothers.js
@@ -1,43 +1,39 @@
-if($('#quote-pop-up').css('display') == 'none'){
-    $('.non-modal').css('opacity', '50%');
-}
-
 $("#name1").click(function () {
     $("#quote-popup-1").removeClass("hidden");
-    //$(".non-modal").addClass("gray-out");
+    $(".non-modal").addClass("gray-out");
 });
 
 $(".close").click(function () {
     $("#quote-popup-1").addClass("hidden");
-    //$(".non-modal").removeClass("gray-out");
+    $(".non-modal").removeClass("gray-out");
 });
 
 $("#name2").click(function () {
     $("#quote-popup-2").removeClass("hidden");
-    //$(".non-modal").addClass("gray-out");
+    $(".non-modal").addClass("gray-out");
 });
 
 $(".close").click(function () {
     $("#quote-popup-2").addClass("hidden");
-    //$(".non-modal").removeClass("gray-out");
+    $(".non-modal").removeClass("gray-out");
 });
 
 $("#name3").click(function () {
     $("#quote-popup-3").removeClass("hidden");
-    //$(".non-modal").addClass("gray-out");
+    $(".non-modal").addClass("gray-out");
 });
 
 $(".close").click(function () {
     $("#quote-popup-3").addClass("hidden");
-    //$(".non-modal").removeClass("gray-out");
+    $(".non-modal").removeClass("gray-out");
 });
 
 $("#name4").click(function () {
     $("#quote-popup-4").removeClass("hidden");
-    //$(".non-modal").addClass("gray-out");
+    $(".non-modal").addClass("gray-out");
 });
 
 $(".close").click(function () {
     $("#quote-popup-4").addClass("hidden");
-    //$(".non-modal").removeClass("gray-out");
+    $(".non-modal").removeClass("gray-out");
 });
